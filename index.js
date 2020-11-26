ed429b495c91fe5d78971c7b7284bafa268407ff
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-26 12:13:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('express')();
+const x = require('express');
 const { log } = console;
 const CORS = {
   'Access-Control-Allow-Origin': '*',
@@ -9,22 +9,25 @@ const hhtml = { 'Content-Type': 'text/html; charset=utf-8' };
 const htxt = { 'Content-Type': 'text/plain; charset=utf-8' };
 
 let port = 80;
-//const app = x();
 
-app
-  .get('/', (req, res) => {
-	log('get/');
-        res
-        .status(200)
-        .set({ hhtml, ...CORS })
-		.send('Hello World!');
-	})
-  .all('/ru', (req, res) => {
-	log('all/ru');
-        res
-        .status(200)
-        .set({ 'Content-Type': 'text/html; charset=utf-8', ...CORS })
-        .send('<h1><i>Yes</i></h1>\n'); 
-  })
-  .listen(process.env.PORT || port, () => log('process.pid ' + process.pid + ' port=' + process.env.PORT));
+function initApp(ex) {
+	const app = ex();
+	app
+		.get('/', (req, res) => {
+			log('get/');
+			res.status(200)
+				.set({ hhtml, ...CORS })
+				.send('Hello World!');
+		})
+		.all('/ru', (req, res) => {
+			log('all/ru');
+				res.status(200)
+				.set({ 'Content-Type': 'text/html; charset=utf-8', ...CORS })
+				.send('<h1><i>Yes</i></h1>\n'); 
+		})
+	return app;
+}
+
+const app = initApp(x);
+app.listen(process.env.PORT || port, () => log('process.pid ' + process.pid + ' port=' + process.env.PORT));
 
