f8c2ab306474fae50c8237c043123dc5c1f45d37
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-26 12:02:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const [{ Server: h1 }, x] = [require('http'), require('express')];
-
+const app = require('express')();
 const { log } = console;
-
 const CORS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE'
@@ -11,7 +9,7 @@ const hhtml = { 'Content-Type': 'text/html; charset=utf-8' };
 const htxt = { 'Content-Type': 'text/plain; charset=utf-8' };
 
 let port = 80;
-const app = x();
+//const app = x();
 
 app
   .get('/', (req, res) => {
@@ -29,27 +27,4 @@ app
         .send('<h1><i>Yes</i></h1>\n'); 
   })
   .listen(process.env.PORT || port, () => log('process.pid ' + process.pid + ' port=' + process.env.PORT));
-//	process.env.PORT || port, () => {  console.log(`Example app listening at http://:${port}`)})
-
-/*app
-  .all('/doc/new/vasya-new.htm', (req, res) => {
-        res
-        .send('<h1><i>Vasya Web Page</i></h1>\n');
-  })
-  .all('/download', (req, res) => {
-        res
-        .status(200)
-        .set({ 
-         'Content-Disposition': 'attachment; filename="file.txt"'
-        })
-        .send('File\n'); 
-  })
-  .use((req, res) => {
-        res
-        .status(404)
-        .set({ 'Content-Type': 'text/html; charset=utf-8', ...CORS })
-        .send('<h1><i>Не найдено!</i></h1>\n');    
-  })
-  .listen(4321);
-*/
 
