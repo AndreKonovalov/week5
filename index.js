6497d753e2e84c01c4095b821918756e0e3daa49
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-27 19:00:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const { log } = console;
 
 const port = 80;
 
-const hashes = crypto.getHashes();
-console.log(hashes); // ['DSA', 'DSA-SHA', 'DSA-SHA1', ...]
-
 import initApp from './app.js';
 const app = initApp(express, bodyParser, createReadStream, crypto, http);
-//const app = initApp(express);
 
 app.listen(process.env.PORT || port, () => log('process.pid ' + process.pid + ' port=' + process.env.PORT))
