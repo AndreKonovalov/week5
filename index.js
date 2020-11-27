b17d46a5edf1fb2132b0fc48b2067d600d1016d6
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-11-27 10:06:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-import x from 'express';
+import express from 'express';
 import crypto from 'crypto';
+import http from 'http';
+import bodyParser from 'body-parser';
+import { createReadStream } from 'fs';
 const { log } = console;
+
+const port = 80;
 
 const hashes = crypto.getHashes();
 console.log(hashes); // ['DSA', 'DSA-SHA', 'DSA-SHA1', ...]
 
-/*let crypto
-try {
-  crypto = require('crypto')
-} catch (err) {
-  console.log('Crypto module is unavailable')
-}*/
-const port = 80;
-
 import initApp from './app.js';
-//const app = initApp(express, bodyParser, createReadStream, crypto, http);
-const app = initApp(x);
+const app = initApp(express, bodyParser, createReadStream, crypto, http);
+//const app = initApp(express);
 
 app.listen(process.env.PORT || port, () => log('process.pid ' + process.pid + ' port=' + process.env.PORT))
-
