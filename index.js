751cde651501a0539b72b7bb00b3ca2505d936e6
AndreKonovalov <47122488+AndreKonovalov@users.noreply.github.com>
2020-12-02 16:55:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import crypto from 'crypto';
 import http from 'http';
 import bodyParser from 'body-parser';
 import { createReadStream } from 'fs';
-
-const port = 80;
-
+import mongoose from 'mongoose';
+import UserModel from './models/User.js';
 import initApp from './app.js';
-const app = initApp(express, bodyParser, createReadStream, crypto, http);
 
-app.listen(process.env.PORT || port);
+const app = initApp(express, bodyParser, createReadStream, crypto, http, mongoose, User);
+app.listen(process.env.PORT || 2345);
